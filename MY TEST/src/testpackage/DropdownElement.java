9466607e223ab31a893f7883eb3bd150e9d737bb
Prem687 <58132743+Prem687@users.noreply.github.com>
2019-12-05 19:22:10 +0530

    Update DropdownElement.java

--- MY TEST/src/testpackage/DropdownElement.java
+++ MY TEST/src/testpackage/DropdownElement.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.Select;
 public class DropdownElement {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method 
+		// TODO Auto-generated method Modified in Git
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
